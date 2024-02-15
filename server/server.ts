252f47cb4b16704be8f52a6233b374821513a77b
NT <git@nabaraj.com>
2024-02-15 09:28:59 -0500

    Initial commit

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+})
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+export default function App() {
+  return (
+    <main>
+      <p>App is running!</p>
+    </main>
+  );
+}
