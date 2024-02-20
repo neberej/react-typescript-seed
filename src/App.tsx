99d9baccc35310a67e2e6bf19de8b6751952534e
NT <git@nabaraj.com>
2024-02-19 23:25:55 -0500

    Add test, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 
+import  React from "react";
 import './App.css';
 
-export default function App() {
-  return (
-    <main>
-      <p>App is running!</p>
-    </main>
-  );
-}
+type AppProps = {
+  title?: string;
+}; /* use `interface` if exporting so that consumers can extend */
+
+const App = ({ title }: AppProps) => <div>{title}</div>;
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-
-// New as of React v18.x
-const root = createRoot(rootElement!);
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <App title="Hello world!"/>
   </StrictMode>
 );
+
